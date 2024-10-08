3fbc0192679cb7ac8baa48d94001edb5a685b30b
kotobusdriver <utkins@gmail.com>
2024-10-08 19:25:10 +0300

    add backend (spring boot)

---
+++ backend/src/test/java/community/TestBackendApplication.java
@@ -0,0 +1,11 @@
+package community;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(BackendApplication::main).with(TestcontainersConfiguration.class).run(args);
+    }
+
+}
