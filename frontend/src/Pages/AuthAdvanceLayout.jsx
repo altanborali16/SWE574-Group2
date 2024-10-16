3d9fbe0abb0ba7866252178bbd6c2a25f7afc4b5
Altan Borali <altan.borali@avl.com>
2024-10-16 21:33:52 +0300

    login page full height

--- frontend/src/Pages/AuthAdvanceLayout.jsx
+++ frontend/src/Pages/AuthAdvanceLayout.jsx
@@ -4,13 +4,11 @@ const AuthAdvanceLayout = ({
   children
 }) => {
   return <>
-      <main>
-        <div className="bg-primary pt-5 pb-0 position-relative">
+        <div className="bg-primary pt-5 pb-0 position-relative" style={{height : "100vh"}}>
           <Container>
             <Row className="justify-content-center text-center">{children}</Row>
           </Container>
         </div>
-      </main>
     </>;
 };
 export default AuthAdvanceLayout;
