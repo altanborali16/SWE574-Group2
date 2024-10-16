ed84dead6f0f408b0e8474bbf64ad075ac02dc61
Enes Akkal <enesakkal48@gmail.com>
2024-10-16 22:47:11 +0300

    update

--- frontend/src/Pages/AuthAdvanceLayout.jsx
+++ frontend/src/Pages/AuthAdvanceLayout.jsx
@@ -5,13 +5,12 @@ import "../Styles/SignIn.css";
 const AuthAdvanceLayout = ({
   children
 }) => {
-  return <>
-      <main>
+  return(
         <div className="bg-secondary pt-5 pb-0 position-relative">
           <Container>
             <Row className="justify-content-center text-center">{children}</Row>
           </Container>
         </div>
-    </>;
+      )
 };
 export default AuthAdvanceLayout;
