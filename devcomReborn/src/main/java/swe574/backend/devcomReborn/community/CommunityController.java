1b639340fdd6160367463993fa12bdc3cc34d93a
efestrikesback <96078086+efestrikesback@users.noreply.github.com>
2024-10-25 22:10:28 +0300

    #82 add get community endpoints

--- devcomReborn/src/main/java/swe574/backend/devcomReborn/community/CommunityController.java
+++ devcomReborn/src/main/java/swe574/backend/devcomReborn/community/CommunityController.java
@@ -1,4 +1,24 @@
 package swe574.backend.devcomReborn.community;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/community")
+@RequiredArgsConstructor
 public class CommunityController {
+    private final CommunityService communityService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Community> getCommunity (@PathVariable Long id){
+        return ResponseEntity.ok(communityService.getCommunity(id));
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<Community> getCommunityList (@PathVariable Long id){
+        return ResponseEntity.ok(communityService.getCommunity(id));
+    }
+
 }
--- devcomReborn/src/main/java/swe574/backend/devcomReborn/community/CommunityService.java
+++ devcomReborn/src/main/java/swe574/backend/devcomReborn/community/CommunityService.java
@@ -1,4 +1,22 @@
 package swe574.backend.devcomReborn.community;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class CommunityService {
+    private final CommunityRepository communityRepository;
+    private final MembershipRepository membershipRepository;
+
+    public Community getCommunity(Long id){
+        return communityRepository.findById(id).orElseThrow();
+    }
+    public List<Community> getCommunityList(){
+        return communityRepository.findAll();
+    }
+
+
 }
